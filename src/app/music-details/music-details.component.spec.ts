437bbbbdb7dc91b39af06653ec5bd8e5db501661
Mritunjay Kumar <mritunjaykumar694@gmail.com>
2021-05-13 02:45:52 +0530

    fix bugs

--- src/app/music-details/music-details.component.spec.ts
+++ src/app/music-details/music-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MusicDetailsComponent } from './music-details.component';
 
@@ -6,7 +6,7 @@ describe('MusicDetailsComponent', () => {
   let component: MusicDetailsComponent;
   let fixture: ComponentFixture<MusicDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MusicDetailsComponent ]
     })
